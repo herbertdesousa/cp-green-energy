b010fc171621fd9be5e37d49ee1f90e131b752fc
herbertdesousa <sousaherbert138@gmail.com>
2024-11-21 10:02:52 -0300

    feat: implement useBalance hook for fetching and formatting balance data

---
+++ src/hooks/useBalance.ts
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+import { AsyncData } from "../types/AsyncData";
+import { httpClient } from "../repository/HttpClient";
+import { delay } from "../utils/delay";
+
+type Balance = {
+  amount: string;
+  closesIn: string;
+}
+
+function parseCurrency(value: string): string {
+  return `R$ ${parseFloat(value).toFixed(2).replace('.', ',')}`;
+}
+
+export function useBalance() {
+  const [balance, setBalance] = useState<AsyncData<Balance>>({
+    type: 'LOADING',
+  });
+
+  useEffect(() => {
+    delay(500).then(() => {
+      httpClient.get<Balance>('/balance')
+        .then(({ data }) => {
+          setBalance({
+            type: 'SUCCESS',
+            data: {
+              amount: parseCurrency(data.amount),
+              closesIn: data.closesIn,
+            },
+          });
+        });
+    });
+  }, []);
+
+  return { balance };
+}
