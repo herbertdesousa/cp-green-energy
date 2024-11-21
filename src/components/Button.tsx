cbbab8ca7d52466e0f14419e3c2f213a0af11945
herbertdesousa <sousaherbert138@gmail.com>
2024-11-21 12:21:21 -0300

    feat: add onClick prop to Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,15 @@
 type Props = {
   children: string;
+  onClick?(): void;
 }
 
-export function Button({ children }: Props) {
+export function Button({ children, onClick }: Props) {
   return (
-    <button type="button" className="text-[#E6FA1E] font-semibold">
+    <button
+      type="button"
+      className="text-[#E6FA1E] font-semibold"
+      onClick={onClick}
+    >
       {children}
     </button>
   );
