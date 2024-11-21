9ffab96beceaab53bcda97f6f49c194048705c5e
herbertdesousa <sousaherbert138@gmail.com>
2024-11-21 11:39:14 -0300

    refactor: useSourcePower to use zustand

--- src/hooks/useSourcePower.ts
+++ src/hooks/useSourcePower.ts
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
-
 import { httpClient } from "../repository/HttpClient";
 import { AsyncData } from "../types/AsyncData";
 import { delay } from "../utils/delay";
+import { create } from "zustand";
 
 type Source = {
   id: string;
@@ -10,17 +9,36 @@ type Source = {
   activeAmount: number;
 }
 
-export function useSourcePower() {
-  const [source, setSource] = useState<AsyncData<Source[]>>({
-    type: 'LOADING',
+type Power = {
+  id: string;
+  label: string;
+  status: boolean;
+};
+
+export type SourceDetails = {
+  id: string;
+  label: string;
+  powers: Power[];
+};
+
+type Actions = {
+  getDetails: (sourceId: string) => Promise<SourceDetails>;
+};
+type States = {
+  source: AsyncData<Source[]>;
+};
+
+export const useSourcePower = create<States & Actions>((set) => {
+  delay(200).then(() => {
+    httpClient.get<Source[]>('/sources')
+      .then(({ data }) => set({ source: { type: 'SUCCESS', data } }));
   });
 
-  useEffect(() => {
-    delay(200).then(() => {
-      httpClient.get<Source[]>('/sources')
-        .then(({ data }) => setSource({ type: 'SUCCESS', data }));
-    });
-  }, []);
-
-  return { source };
-}
+  return {
+    source: { type: 'LOADING' },
+    getDetails: async (sourceId) => {
+      const { data } = await httpClient.get<SourceDetails>(`/sources/${sourceId}`);
+      return data;
+    }
+  };
+});
